c6421d34d4521a264848cd62f932d16a4025c5e9
Steven Perfors <stevecanbuild@gmail.com>
2020-04-17 13:38:20 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,9 +4,10 @@ class Dog
   @@all = []
   
   def initialize(name, breed, age)
-    @name = name 
-    @breed = breed
-    @age = age
+    self.name = name 
+    self.breed = breed
+    self.age = age
+    self.all = 
   end
   
 end
